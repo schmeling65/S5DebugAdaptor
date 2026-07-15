4c374135e2d312a5a6f25ef69a986b25f1baea10
schmeling65 <markusrumpel2@gmail.com>
2026-07-15 16:14:41 +0200

    cleanup of extension directory, back to default

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -1,32 +1,26 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import { spawn } from 'child_process';
 import * as vscode from 'vscode';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	context.subscriptions.push(vscode.debug.registerDebugAdapterDescriptorFactory('s5lua', new S5DebugAdapterDescriptorFactory()));
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "extension" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	const disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello World from extension!');
+	});
+
+	context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() { }
-
-class S5DebugAdapterDescriptorFactory implements vscode.DebugAdapterDescriptorFactory {
-	async createDebugAdapterDescriptor(session: vscode.DebugSession, executable: vscode.DebugAdapterExecutable | undefined): Promise<vscode.ProviderResult<vscode.DebugAdapterDescriptor>> {
-		let conf = session.configuration;
-		if (conf.request === "launch" && conf.program) {
-			let del = 1;
-			if (conf.attachDelay) {
-				del = conf.attachDelay;
-			}
-			spawn(conf.program, conf.args);
-			await this.sleep(del * 1000);
-		}
-		return new vscode.DebugAdapterServer(19021);
-	}
-	sleep(ms: number) {
-		return new Promise(resolve => setTimeout(resolve, ms));
-	}
-}
+export function deactivate() {}
