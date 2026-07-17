e677a9642f82504587b18e5fdc5df7d4a79affc2
schmeling65 <markusrumpel2@gmail.com>
2026-07-17 20:36:24 +0200

    persistent auto search check through visibility change fixed

--- extension/src/webview/webview.ts
+++ extension/src/webview/webview.ts
@@ -1,8 +1,6 @@
 import * as vscode from 'vscode'
 import { S5DebugAdapterDescriptorFactory } from '../debugadaptor/debugadaptor';
 
-type vsCodeDisposable = vscode.Disposable | undefined
-
 interface messageContent {
     type: string,
     value: boolean,
@@ -11,12 +9,15 @@ interface messageContent {
 export class MySidebarProvider implements vscode.WebviewViewProvider {
     private isCheckboxChecked = false;
     private persistentStorageKeyName = "IsS5LuaDebuggerActive"
-    private registerdDebugAdapterDescriptorFactory: vsCodeDisposable =  undefined
+    private registerdDebugAdapterDescriptorFactory: vscode.Disposable
     private S5DebugAdapterDescriptorFactoryDisposable: S5DebugAdapterDescriptorFactory
     constructor(private readonly extensionContext: vscode.ExtensionContext,) {
         let checkboxChecked: boolean = this.extensionContext.globalState.get(this.persistentStorageKeyName)!;
         this.isCheckboxChecked = checkboxChecked
         this.S5DebugAdapterDescriptorFactoryDisposable = new S5DebugAdapterDescriptorFactory()
+        this.registerdDebugAdapterDescriptorFactory = vscode.debug.registerDebugAdapterDescriptorFactory('s5lua', this.S5DebugAdapterDescriptorFactoryDisposable)
+        this.extensionContext.subscriptions.push(this.registerdDebugAdapterDescriptorFactory);
+        console.log("webview created")
     }
 
     public async resolveWebviewView( webviewView: vscode.WebviewView, context: vscode.WebviewViewResolveContext, token: vscode.CancellationToken) {
@@ -33,23 +34,31 @@ export class MySidebarProvider implements vscode.WebviewViewProvider {
             switch (message.type) {
                 case 'toggleChanged': {
                     await this.extensionContext.globalState.update(this.persistentStorageKeyName, message.value);
+                    this.isCheckboxChecked = message.value
                      this.activateOrStopGameSearch(message.value)
                     break;
                 }
             }
         });
+        webviewView.onDidChangeVisibility(() => {
+        if (webviewView.visible) {
+             webviewView.webview.postMessage({
+                command:"setCheckboxToTrue",
+                value:this.isCheckboxChecked
+            })
+        } else {
+            console.log("Webview wurde verlassen oder minimiert");
+            // Hier Code ausführen, wenn der Nutzer wegschaltet
+        }
+    });
     }
 
     private activateOrStopGameSearch(status: boolean) {
         if (status) {
-            this.registerdDebugAdapterDescriptorFactory = vscode.debug.registerDebugAdapterDescriptorFactory('s5lua', this.S5DebugAdapterDescriptorFactoryDisposable)
-            this.extensionContext.subscriptions.push(this.registerdDebugAdapterDescriptorFactory);
             this.S5DebugAdapterDescriptorFactoryDisposable.searchForGame()
         }
         else {
             this.S5DebugAdapterDescriptorFactoryDisposable.stopSearchForGame()
-            this.registerdDebugAdapterDescriptorFactory!.dispose()
-            this.registerdDebugAdapterDescriptorFactory = undefined
         }
     }
 
